31f34ba116eec6acb3b80b20dd4781642f2bb0c3
JamieDeMaria <jamie@elementl.com>
2022-07-28 17:42:21 -0400

    following tutorial

---
+++ hacker_news/hacker_news/repo.py
@@ -0,0 +1,55 @@
+from hacker_news.assets import items, comments, stories
+from hacker_news.snowflake import clone_prod
+from dagster import repository, with_resources
+from dagster_snowflake import build_snowflake_io_manager, snowflake_resource
+from dagster_snowflake_pandas import SnowflakePandasTypeHandler
+import os
+
+snowflake_io_manager = build_snowflake_io_manager([SnowflakePandasTypeHandler()])
+
+
+@repository
+def repo():
+    snowflake_config = {
+        "account": {"env": "SNOWFLAKE_ACCOUNT"},
+        "user": {"env": "SNOWFLAKE_USER"},
+        "password": {"env": "SNOWFLAKE_PASSWORD"},
+        "database": "BOLLINGER"
+    }
+    resource_defs = {
+        "branch": {
+            "snowflake_io_manager": snowflake_io_manager.configured(
+                {
+                    **snowflake_config,
+                    "schema": f"PRODUCTION_CLONE_{os.getenv('DAGSTER_CLOUD_PULL_REQUEST_ID')}",
+                }
+            ),
+            "snowflake": snowflake_resource.configured(
+            {
+                **snowflake_config,
+                "schema": f"PRODUCTION_CLONE_{os.getenv('DAGSTER_CLOUD_PULL_REQUEST_ID')}",
+            }
+        ),
+        },
+        "production": {
+            "snowflake_io_manager": snowflake_io_manager.configured(
+                {
+                    **snowflake_config,
+                    "schema": "PRODUCTION"
+                }
+            ),
+            "snowflake": snowflake_resource.configured({**snowflake_config, "schema": "PRODUCTION"}),
+
+        },
+    }
+
+    def get_current_env():
+        is_branch_depl = os.getenv("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT")
+        assert is_branch_depl != None  # env var must be set
+        return "branch" if is_branch_depl else "prod"
+
+    return [
+        with_resources([items, comments, stories], resource_defs=resource_defs[get_current_env()]),
+        clone_prod.to_job(resource_defs=resource_defs[get_current_env()]),
+
+    ]
---
+++ hacker_news/hacker_news/snowflake.py
@@ -0,0 +1,20 @@
+from dagster import op, graph, In, Nothing
+import os
+
+@op(required_resource_keys={"snowflake"})
+def drop_database_clone(context):
+    context.resources.snowflake.execute_query(
+        f"DROP SCHEMA IF EXISTS PRODUCTION_CLONE_{os.environ['DAGSTER_CLOUD_PULL_REQUEST_ID']}"
+    )
+
+
+@op(required_resource_keys={"snowflake"}, ins={"start": In(Nothing)})
+def clone_production_database(context):
+    context.resources.snowflake.execute_query(
+        f"CREATE SCHEMA PRODUCTION_CLONE_{os.environ['DAGSTER_CLOUD_PULL_REQUEST_ID']} CLONE \"PRODUCTION\""
+    )
+
+
+@graph
+def clone_prod():
+    clone_production_database(start=drop_database_clone())
