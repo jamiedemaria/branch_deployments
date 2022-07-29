8a8f6d0ad234362898af23075641a99240064e0a
JamieDeMaria <jamie@elementl.com>
2022-07-29 14:59:23 -0400

    more logging

--- hacker_news/hacker_news/snowflake.py
+++ hacker_news/hacker_news/snowflake.py
@@ -4,6 +4,8 @@ import os
 @op(required_resource_keys={"snowflake"})
 def drop_database_clone(context):
     context.log.info(f"SNOWFLAKE_ACCOUNT: {os.getenv('SNOWFLAKE_ACCOUNT')}")
+    context.log.info(f"SNOWFLAKE_USER: {os.getenv('SNOWFLAKE_USER')}")
+    context.log.info(f"SNOWFLAKE_PASSWORD: {os.getenv('SNOWFLAKE_PASSWORD')}")
     context.resources.snowflake.execute_query(
         f"DROP SCHEMA IF EXISTS PRODUCTION_CLONE_{os.environ['DAGSTER_CLOUD_PULL_REQUEST_ID']}"
     )
